b4ced207f78db4fad046cc4eeb8dd4b31c69901c
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-10-03 18:34:54 +0000

    Updated wxFormBuilder.workspace to include the premake generated Code::Blocks Projects. Updated Changelog.txt with progress towards 3.0 beta. Updated the dependencies in all the plug-ins. Cleaned the plugin directory a bit.
    git-svn-id: svn://svn.code.sf.net/p/wxformbuilder/code/2.x/trunk@504 1bf5dfe7-1b0e-0410-9632-deacb7090205

--- sdk/plugin_interface/premake.lua
+++ sdk/plugin_interface/premake.lua
@@ -1,19 +1,19 @@
-package.name = "Plugin_Interface"
+package.name = "Plugin Interface"
 package.kind = "lib"
 package.language = "c++"
 package.files = { matchfiles( "*.h", "*.cpp" ) }
 
 -- Not needed soon brcause of a bug fix.
-if ( options[target] == "cb-gcc" ) then
+--if ( options[target] == "cb-gcc" ) then
     -- Change the default lib extention to .a instead of .lib
-    package.targetextension = "a"
-end
+    --package.targetextension = "a"
+--end
 
 -- Set object output directory.
 package.config["Debug"].objdir = ".objsd"
 package.config["Debug (Unicode)"].objdir = ".objsud"
 package.config["Release"].objdir = ".objs"
-package.config["Release (Unicode)"].objdir = { "$(WXWIN)/include", "../tinyxml" }
+package.config["Release (Unicode)"].objdir = ".objsu"
 
 -- Set the targets.
 package.config["Debug"].target = "fbPluginInterfaced"
@@ -22,18 +22,20 @@ package.config["Release"].target = "fbPluginInterface"
 package.config["Release (Unicode)"].target = "fbPluginInterfaceu"
 
 -- Set the build options for the Unicode build Targets.
+package.buildflags = { "extra-warnings" }
 package.config["Debug (Unicode)"].buildflags = { "unicode" }
-package.config["Release (Unicode)"].buildflags = { "no-symbols", "optimize", "unicode" }
+package.config["Release"].buildflags = { "no-symbols", "optimize-speed" }
+package.config["Release (Unicode)"].buildflags = { "unicode", "no-symbols", "optimize-speed" }
 
 -- Set include paths
 package.includepaths = { "$(WXWIN)/include", "../tinyxml" }
 
 -- Set defines.
 if ( OS == "windows") then
-	package.config["Debug"].defines = { "DEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "__WXDEBUG__", "TIXML_USE_TICPP" }
-	package.config["Debug (Unicode)"].defines = { "DEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "__WXDEBUG__", "TIXML_USE_TICPP", "UNICODE", "_UNICODE", "wxUSE_UNICODE" }
-	package.config["Release"].defines = { "NDEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "TIXML_USE_TICPP" }
-	package.config["Release (Unicode)"].defines = { "NDEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "TIXML_USE_TICPP", "UNICODE", "_UNICODE", "wxUSE_UNICODE" }
+	package.config["Debug"].defines = { "DEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "__WXDEBUG__", "TIXML_USE_TICPP", "WXUSINGDLL" }
+	package.config["Debug (Unicode)"].defines = { "DEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "__WXDEBUG__", "TIXML_USE_TICPP", "UNICODE", "_UNICODE", "WXUSINGDLL" }
+	package.config["Release"].defines = { "NDEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "TIXML_USE_TICPP", "WXUSINGDLL" }
+	package.config["Release (Unicode)"].defines = { "NDEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "TIXML_USE_TICPP", "UNICODE", "_UNICODE", "WXUSINGDLL" }
 else
 	package.config["Debug"].defines = { "DEBUG", "__WX__", "__WXDEBUG__", "TIXML_USE_TICPP", "WXUSINGDLL" }
 	package.config["Release"].defines = { "NDEBUG", "__WX__", "TIXML_USE_TICPP", "WXUSINGDLL" }
